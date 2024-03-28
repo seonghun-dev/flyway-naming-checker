6279c0e6a3cddf44f114e1e8ee05a724d121816c
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2024-03-28 14:49:48 +0900

    get file list

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
-use std::fs::write;
+use std::fs;
+use std::io::{self, Write};
 use std::process::exit;
 
 fn main() {
@@ -9,10 +10,38 @@ fn main() {
     let path = &args[1];
 
     if !path.is_empty() {
-        eprintln!("files: {path}");
-        write(github_output_path, format!("files={path}")).unwrap();
-        exit(0);
+        if let Ok(files) = get_file_list(path) {
+            eprintln!("files: {:?}", files);
+
+            if let Err(err) = write(&github_output_path, format!("files={:?}", files)) {
+                eprintln!("Error writing output: {}", err);
+                exit(1);
+            } else {
+                exit(0);
+            }
+        } else {
+            eprintln!("Error reading directory");
+            exit(1);
+        }
     } else {
+        eprintln!("No path provided");
         exit(1);
     }
+}
+
+fn get_file_list(path: &str) -> io::Result<Vec<String>> {
+    let mut files = Vec::new();
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        if let Some(file_name) = entry.file_name().to_str() {
+            files.push(file_name.to_owned());
+        }
+    }
+    Ok(files)
+}
+
+fn write(output_path: &str, content: String) -> io::Result<()> {
+    let mut file = fs::File::create(output_path)?;
+    file.write_all(content.as_bytes())?;
+    Ok(())
 }
